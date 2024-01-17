bf01108af6c05018bccacfe5724a725062baad4e
Aarteagaj <anabelaj91@gmail.com>
2024-01-17 15:33:25 -0500

    Se agrega documentacion de metodos

--- src/models/report_processing/report_builder.py
+++ src/models/report_processing/report_builder.py
@@ -10,6 +10,15 @@ class ReportBuilder:
 
     @classmethod
     def from_path(cls, origin_path: str) -> str:
+        '''
+        To read files from path and merge content in str
+
+        Parameters:
+        - origin_path: path where the files to read are located
+        Returns:
+            str
+        '''
+
         list_files = os.listdir(origin_path)
         content_merge = ''
         for file in list_files:
@@ -20,7 +29,17 @@ class ReportBuilder:
             cls.move_files(origin_path, 'backup', file) # TODO: get target path from config file
         return content_merge
 
-    def move_files(self, origin_path: str, target_path: Path, filename: str) -> None:
+    def move_files(self, origin_path: str, target_path: str, filename: str) -> None:
+        '''
+        To read files from path and merge content in str
+
+        Parameters:
+        - origin_path: path where the files to read are located
+        - target_path: path where the file will be moved
+        - filename: file name
+        Returns:
+            None
+        '''
         Path(origin_path + '/' + filename).rename(target_path + '/' + filename)
 
     def show_report():
