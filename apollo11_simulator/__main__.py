148e0873b232197bb0f78bc763a3c1b9430eb8e1
Aarteagaj <anabelaj91@gmail.com>
2024-01-25 18:53:16 -0500

    configuracion inicial

--- apollo11_simulator/__main__.py
+++ apollo11_simulator/__main__.py
@@ -1,10 +1,13 @@
 import argparse
-
 from apollo11_simulator.models.event_processing.event_manager import EventManager
 from apollo11_simulator.models.report_processing.report_builder import ReportBuilder
+from apollo11_simulator.config import config
+
 
 # logica argparse
 if __name__ == '__main__':
+    event_params = config["event_params"]
+
     parser = argparse.ArgumentParser(description='Generador de eventos y reportes en la línea de comandos.')
 
     parser.add_argument('operation', choices=['generate-events', 'generate-report'],
@@ -15,7 +18,10 @@ if __name__ == '__main__':
     match args.operation:
             case 'generate-events':
                 print('Running in "generate events" mode')
-                event_manager = EventManager(input_data_file = 'input_data/simulation.json', target_path='devices', frequency_seconds=3, range_of_files=(7, 10))
+                event_manager = EventManager(input_data_file = event_params["input_data_file"],
+                                             target_path= event_params["target_path"],
+                                             frequency_seconds= event_params["frequency_seconds"],
+                                             range_of_files= (event_params["range_of_files"]["min"], event_params["range_of_files"]["max"]))
                 event_manager()
             case 'generate-report':
                 print('Running in "generate report" mode')
